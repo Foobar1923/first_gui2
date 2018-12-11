d910e26a96e3ea106000b098780a27e8aba8e7ae
Foobar1923 <16374806+Foobar1923@users.noreply.github.com>
2018-12-10 20:35:20 -0600

    final commit

---
+++ first_gui/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace first_gui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickMe_Click(object sender, EventArgs e)
+        {
+            ClickMe.Text = "Goodbye Kind World";
+            timer1.Enabled = true;
+        }
+                
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
